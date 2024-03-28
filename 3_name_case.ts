ee1ea9e2e72fdf1fba8a081779aa69cd16a92793
maazsheikh13 <165181534+maazsheikh13@users.noreply.github.com>
2024-03-28 20:05:10 +0500

    Add files via upload

---
+++ 3_name_case.ts
@@ -0,0 +1,17 @@
+let personname :string = '';
+
+personname= prompt('what is your name?')||'';
+
+let lowercase :string = personname.toLowerCase() ;
+let uppercase :string = personname.toUpperCase() ;
+let titlecase :string = personname.split('').map(word=>word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('')
+
+if(personname !== null && personname !== ''){
+    alert(`Hello ${personname}, Here are your name in:
+    Lowercase: ${lowercase}
+    Uppercase: ${uppercase }
+    Titlecase: ${titlecase}`)
+}
+else{
+    alert('Please fill your name !')
+}
