d08f9653334a84905940de949322bf8a7d74a913
maazsheikh13 <165181534+maazsheikh13@users.noreply.github.com>
2024-03-28 20:06:38 +0500

    Add files via upload

---
+++ 4 Famous_quote.ts
@@ -0,0 +1,1 @@
+console.log('Altaf bhai said, “karachi chal”');
---
+++ 5_famous_quote.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'albert einestine'
+
+let message :string = '"experience is source of knowledg."';
+
+console.log(`${ famous_person} one said, ${message}`);
